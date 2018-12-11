2eb10ae45f456b2e6e82642af68a5fd8842bf85e
Khayyam.Abishov <xayyam.abisov@gmail.com>
2018-12-12 01:41:09 +0400

    Add project files.

---
+++ ImportExcelDatabase/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ImportExcelDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(225, 175);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 27);
+            this.btnChoose.TabIndex = 2;
+            this.btnChoose.Text = "Choose";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(358, 175);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnChoose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
---
+++ ImportExcelDatabase/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImportExcelDatabase
+{
+    public partial class Form1 : Form
+    {
+       static OpenFileDialog od = new OpenFileDialog();
+        string excelFilePath = od.FileName;
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+
+        }
+
+        private void InsertExcelRecords()
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+
+            //  ExcelConn(_path);  
+            string constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES;""", excelFilePath);
+            OleDbConnection Econ = new OleDbConnection(constr);
+                string Query = string.Format("Select [StudentId] [StudentName],[StudentAge] FROM [{0}]", "Sheet1$");
+                OleDbCommand Ecom = new OleDbCommand(Query, Econ);
+                Econ.Open();
+
+                DataSet ds = new DataSet();
+                OleDbDataAdapter oda = new OleDbDataAdapter(Query, Econ);
+                Econ.Close();
+                oda.Fill(ds);
+                DataTable Exceldt = ds.Tables[0];
+
+                for (int i = Exceldt.Rows.Count - 1; i >= 0; i--)
+                {
+                    if (Exceldt.Rows[i]["Employee Name"] == DBNull.Value || Exceldt.Rows[i]["Email"] == DBNull.Value)
+                    {
+                        Exceldt.Rows[i].Delete();
+                    }
+                }
+                Exceldt.AcceptChanges();
+                //creating object of SqlBulkCopy      
+                System.Data.SqlClient.SqlBulkCopy objbulk = new System.Data.SqlClient.SqlBulkCopy(sqlConnection);
+                //assigning Destination table name      
+                objbulk.DestinationTableName = "Student";
+                //Mapping Table column    
+                objbulk.ColumnMappings.Add("StudentId", "StudentId");
+                objbulk.ColumnMappings.Add("StudentName", "StudentName");
+                objbulk.ColumnMappings.Add("StudentAge", "StudentAge");
+              
+
+                //inserting Datatable Records to DataBase   
+                sqlConnection.ConnectionString = "server = VSBS01; database = dbHRVeniteck; User ID = sa; Password = veniteck@2016"; //Connection Details    
+            sqlConnection.Open();
+                objbulk.WriteToServer(Exceldt);
+            sqlConnection.Close();
+                MessageBox.Show("Data has been Imported successfully.", "Imported", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            
+         
+
+        }
+      
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (od.ShowDialog()==DialogResult.OK)
+            {
+                string excelFilePath = od.FileName;
+
+            }
+        }
+    }
+}
